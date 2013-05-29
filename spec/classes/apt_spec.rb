5685075ce910812681bb741ecbc02a6f6ec16718
Pete de Zwart <pdzwart@atlassian.com>
2013-05-29 13:01:15 +1000

    PERF-1259: Moved rspec collection map outside of if blocks.

--- spec/classes/apt_spec.rb
+++ spec/classes/apt_spec.rb
@@ -2,19 +2,31 @@ require 'spec_helper'
 
 describe 'apt' do
   context 'default' do
-    it {
-      should contain_file('/etc/apt/apt.conf').with({
-        'ensure' => 'file',
-        'owner'  => 'root',
-        'group'  => 'root',
-        'mode'   => '0644',
-      })
+    files = [
+      '/etc/apt/apt.conf'
+    ]
+
+    packages = [
+      'apt'
+    ]
+
+    files.map {
+      |x| it {
+        should contain_file(x).with({
+          'ensure' => 'file',
+          'owner'  => 'root',
+          'group'  => 'root',
+          'mode'   => '0644',
+        })
+      }
     }
 
-    it {
-      should contain_package('apt').with({
-        'ensure' => 'installed'
-      })
+    packages.map {
+      |x| it {
+        should contain_package(x).with({
+          'ensure' => 'installed'
+        })
+      }
     }
   end
 end
